feede8021002fb93b5064538883e80264166c002
sebas-ib <ibarraperezsebastian@gmail.com>
2025-02-07 12:45:29 -0800

    Added services section

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
+document.addEventListener("DOMContentLoaded", () => {
+  // Hamburger menu
+  function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
 
-  // Toggle the menu and icon classes
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+    if (menu && icon) {
+      menu.classList.toggle("open");
+      icon.classList.toggle("open");
 
-  // Check if the menu is open to adjust body overflow
-  if (menu.classList.contains("open")) {
-    document.body.style.overflow = "hidden"; // Disable scrolling when menu is open
-  } else {
-    document.body.style.overflow = "auto"; // Enable scrolling when menu is closed
+      document.body.style.overflow = menu.classList.contains("open")
+        ? "hidden"
+        : "auto";
+    }
   }
-}
-
-$(document).ready(() => {
-  // Bind the toggleMenu function to the hamburger icon click event
-  $(".hamburger-icon").on("click", toggleMenu);
 });
