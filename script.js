abde19b8c1b4008129dfa61cc2189dd3f6cbf685
sebas-ib <ibarraperezsebastian@gmail.com>
2025-02-06 21:01:44 -0800

    Improved website UI

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
 
+  // Toggle the menu and icon classes
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+
+  // Check if the menu is open to adjust body overflow
+  if (menu.classList.contains("open")) {
+    document.body.style.overflow = "hidden"; // Disable scrolling when menu is open
+  } else {
+    document.body.style.overflow = "auto"; // Enable scrolling when menu is closed
+  }
 }
+
+$(document).ready(() => {
+  // Bind the toggleMenu function to the hamburger icon click event
+  $(".hamburger-icon").on("click", toggleMenu);
+});
