08000d1cca0c1f28dedb198168085205cf3803a1
sebas-ib <ibarraperezsebastian@gmail.com>
2025-03-24 13:05:08 -0700

    Fixed navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,10 +11,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-        />
         <title>IBA Construction</title>
       </head>
       <body>
